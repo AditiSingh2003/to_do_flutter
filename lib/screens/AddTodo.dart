f24cba5e6cac3bcfff08e213f1809792dcc87cb7
Aditi <adithakurti@gmail.com>
2023-06-06 16:40:56 +0530

    Added App icon

--- lib/screens/AddTodo.dart
+++ lib/screens/AddTodo.dart
@@ -2,7 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-
 import 'home.dart';
 
 class AddTodo extends StatefulWidget {
